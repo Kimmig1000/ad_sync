9a789d57c18b1d0611cdc63b3bd60e6f5446109f
Kimmig1000 <martinkimmig90@gmail.com>
2023-01-28 13:41:57 +0100

    added material ui

---
+++ pages/inbound.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Inbound: NextPage = () => {
+  return <h2>test</h2>;
+};
+
+export default Inbound;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
@@ -13,24 +14,27 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>Welcome to AD Sync</h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          App zur Automatisierung von AD Synchrnoisation
           <code className={styles.code}>pages/index.tsx</code>
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <Link href="/inbound" className={styles.card}>
+            <a className={styles.card}>
+              <h2>Eingangs-Daten &rarr;</h2>
+              <p>Konfigurieren welche Daten eingelesen werden sollen</p>
+            </a>
+          </Link>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          <a className={styles.card}>
+            <h2>AD Konfiguration &rarr;</h2>
+            <p>
+              Konfiguriere wie die Eingangs-Daten auf AD Felder übertragen
+              werden soll
+            </p>
           </a>
 
           <a
@@ -59,14 +63,14 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
